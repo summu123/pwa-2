f133085cbad0b428751741815bd21e30d84caa56
summu123 <sumanthkotha1999@gmail.com>
2019-03-30 16:14:14 +0530

    first commit

---
+++ js/sw.js
@@ -0,0 +1,32 @@
+this.addEventListener('install',function(event){
+  event.waitUntil(
+    caches.open('mycache').then(function(e){
+        e.addAll([
+          '/index.html',
+          '/css/index.css',
+          '/form.html',
+          '/css/form.css',
+          '/js/main.js',
+          '/resume.html',
+          '/css/resume.css',
+          '/js/get.js',
+          '/js/resume.js'
+
+      ])
+    })
+  )
+})
+this.addEventListener('fetch',function(event){
+  event.respondWith(caches.open('mycache')
+.then(function(cache){
+  return cache.match(event.request)
+  .then(function(result){
+    return result || fetch(event.request)
+    .then(function(result){
+      cache.put(event.request,result.clone());
+      return result;
+    })
+  })
+})
+)
+})
